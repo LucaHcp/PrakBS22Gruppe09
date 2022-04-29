675a8fc3e0bb3a8f2bc06b573ae19cd9ed989d01
@lhaucap <luca_marten.haucap@smail.th-koeln.de>
2022-04-29 11:48:26 +0200

    ECHO REQUEST

--- main.c
+++ main.c
@@ -1,56 +1,86 @@
+/*******************************************************************************
+
+  Ein TCP-Echo-Server als iterativer Server: Der Server schickt einfach die
+  Daten, die der Client schickt, an den Client zurück.
+
+*******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "LinkedListHeader.h"
-#include "Client.h"
-#include "Server.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 
-int main(){
+#define BUFSIZE 1024 // Größe des Buffers
+#define ENDLOSSCHLEIFE 1
+#define PORT 5678
 
-    //serverMain();
-    //clientMain();
 
-    char serverMessage[SERVER_BUFF_SIZE] = "You have reached the Server!";
+int main() {
 
-    // create the server socket
-    int serverSocket;
-    serverSocket = socket(AF_INET,SOCK_STREAM,0);
+    int rfd; // Rendevouz-Descriptor
+    int cfd; // Verbindungs-Descriptor
 
-    //Define the server address
-    struct sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(SERVER_PORT);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in client; // Socketadresse eines Clients
+    socklen_t client_len; // Länge der Client-Daten
+    char in[BUFSIZE]; // Daten vom Client an den Server
+    int bytes_read; // Anzahl der Bytes, die der Client geschickt hat
 
-    //Bind the socket to our specified IP and port
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
-    listen(serverSocket,5);
-
-    //Create Integer hold the client socket
-    int clientSocket;
-    clientSocket = accept(serverSocket,NULL,NULL);
-
-    printf("TEST1");
-
-    //send the message
-    send(clientSocket , serverMessage , sizeof(serverMessage),0);
-
-    printf("TEST2");
-
-    char quit[4] = "quit";
-
-    while(1) {
-        printf("TEST3");
-        char input[SERVER_BUFF_SIZE];
-        read(serverSocket,&input,SERVER_BUFF_SIZE);
-
+    // Socket erstellen
+    rfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (rfd < 0 ){
+        fprintf(stderr, "socket konnte nicht erstellt werden\n");
+        exit(-1);
     }
 
-    // Close the socket
-    close(serverSocket);
 
-    return 0;
+    // Socket Optionen setzen für schnelles wiederholtes Binden der Adresse
+    int option = 1;
+    setsockopt(rfd, SOL_SOCKET, SO_REUSEADDR, (const void *) &option, sizeof(int));
 
-    return 0;
+
+    // Socket binden
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+    int brt = bind(rfd, (struct sockaddr *) &server, sizeof(server));
+    if (brt < 0 ){
+        fprintf(stderr, "socket konnte nicht gebunden werden\n");
+        exit(-1);
+    }
+
+
+    // Socket lauschen lassen
+    int lrt = listen(rfd, 5);
+    if (lrt < 0 ){
+        fprintf(stderr, "socket konnte nicht listen gesetzt werden\n");
+        exit(-1);
+    }
+
+    while (ENDLOSSCHLEIFE) {
+
+        // Verbindung eines Clients wird entgegengenommen
+        cfd = accept(rfd, (struct sockaddr *) &client, &client_len);
+
+        // Lesen von Daten, die der Client schickt
+        bytes_read = read(cfd, in, BUFSIZE);
+
+        // Zurückschicken der Daten, solange der Client welche schickt (und kein Fehler passiert)
+        while (bytes_read > 0) {
+            printf("sending back the %d bytes I received...\n", bytes_read);
+
+            write(cfd, in, bytes_read);
+            bytes_read = read(cfd, in, BUFSIZE);
+
+        }
+        close(cfd);
+    }
+
+    // Rendevouz Descriptor schließen
+    close(rfd);
+
 }
